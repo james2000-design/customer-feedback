0fe88682c360c7717c3c7c6f2d442228b88755b8
James <gbakonjames800@gmail.com>
2024-06-20 15:32:32 +0100

    fehhh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,40 @@
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Header({ text, bgColor, textColor }) {
   const headerStyles = {
     backgroundColor: bgColor,
     color: textColor,
-  }
+  };
 
   return (
     <header style={headerStyles}>
-      <div className='container'>
-        <Link to='/' style={{ textDecoration: 'none', color: '#ff6a95' }}>
+      <div className="container">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#fff",
+            display: "flex",
+            justifyContent: "center",
+          }}>
           <h2>{text}</h2>
         </Link>
       </div>
     </header>
-  )
+  );
 }
 
 Header.defaultProps = {
-  text: 'Feedback UI',
-  bgColor: 'rgba(0,0,0,0.4)',
-  textColor: '#ff6a95',
-}
+  text: "Customer Feedback",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#fff",
+};
 
 Header.propTypes = {
   text: PropTypes.string,
   bgColor: PropTypes.string,
   textColor: PropTypes.string,
-}
+};
 
-export default Header
+export default Header;
